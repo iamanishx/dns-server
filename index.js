f8988c6272bfdf7be6ffc18fbc0963fd57ffd079
iamanishx <manishbiswal754@gmail.com>
2025-01-10 09:10:15 +0530

    INITAL DONE

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const dgram = require('node:dgram');
 const dnsPacket = require('dns-packet');
-
+const { type } = require('node:os');
 const server = dgram.createSocket('udp4')
+
+const db = {
+   'www.google.com':{
+     type: 'A',  
+     data: '1.2.3.4'
+   },
+   'www.facebook.com': {
+      type: 'A',
+      data: '4.5.6.7'
+   },
+   'www.youtube.com': {
+      type: 'A',
+      data: '7.8.9.10'
+   }
+}
 
 server.on('message',(msg, rinfo)=>{
    const incomingReq = dnsPacket.decode(msg);
-   console.log({
-    question: incomingReq.questions,
-    rinfo
-   })
+   const ipFromDb = db[incomingReq.questions[0].name];
+
+   const ans = dnsPacket.encode  ({
+      type:'response',  
+      id: incomingReq.id,
+      flags: dnsPacket.AUTHORITATIVE_ANSWER,
+      questions: incomingReq.questions,
+      answers: [{
+         name: incomingReq.questions[0].name,
+         type: ipFromDb.type,
+         class: 'IN',
+         ttl: 300,
+         data: ipFromDb.data,
+      }],
 })
 
-server.bind(53, () => console.log('My DNS-SERVER is running on port 53'))
+server.send(ans, rinfo.port, rinfo.address, (err) => {
+   if (err) {
+      console.log(err)
+   }
+})
+})
+
+server.bind(5300, () => console.log('My DNS-SERVER is running on port 5300'))
